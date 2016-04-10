c03cdf287e795da69a2dbeb21b984e7cccc6b501
Pavel Čermák <admin@cermakpavel.com>
2016-04-10 20:48:58 +0200

    Repair problem with signInForm

--- app/forms/SignFormFactory.php
+++ app/forms/SignFormFactory.php
@@ -1,28 +1,20 @@
 <?php
-
 namespace App\Forms;
-
 use Nette;
 use Nette\Application\UI\Form;
 use Nette\Security\User;
-
 
 class SignFormFactory extends Nette\Object
 {
 	/** @var FormFactory */
 	private $factory;
-
 	/** @var User */
 	private $user;
-
-
 	public function __construct(FormFactory $factory, User $user)
 	{
 		$this->factory = $factory;
 		$this->user = $user;
 	}
-
-
 	/**
 	 * @return Form
 	 */
@@ -31,19 +23,13 @@ class SignFormFactory extends Nette\Object
 		$form = $this->factory->create();
 		$form->addText('username', 'Username:')
 			->setRequired('Please enter your username.');
-
 		$form->addPassword('password', 'Password:')
 			->setRequired('Please enter your password.');
-
 		$form->addCheckbox('remember', 'Keep me signed in');
-
 		$form->addSubmit('send', 'Sign in');
-
 		$form->onSuccess[] = array($this, 'formSucceeded');
 		return $form;
 	}
-
-
 	public function formSucceeded(Form $form, $values)
 	{
 		if ($values->remember) {
@@ -51,12 +37,10 @@ class SignFormFactory extends Nette\Object
 		} else {
 			$this->user->setExpiration('20 minutes', TRUE);
 		}
-
 		try {
 			$this->user->login($values->username, $values->password);
 		} catch (Nette\Security\AuthenticationException $e) {
 			$form->addError('The username or password you entered is incorrect.');
 		}
 	}
-
 }
