9c19226c64238bad5e047325ed29a0af1cc7fedf
Pavel Čermák <admin@cermakpavel.com>
2016-04-16 22:48:51 +0200

    Update code format

--- app/model/UserManager.php
+++ app/model/UserManager.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace App\Model;
 
 use Nette;
-use Nette\Security\Passwords;
 use Nette\Security as NS;
-
+use Nette\Security\Passwords;
 
 /**
  * Users management.
@@ -19,19 +17,17 @@ class UserManager extends Nette\Object implements NS\IAuthenticator
 		COLUMN_PASSWORD_HASH = 'password',
 		COLUMN_ROLE = 'role';
 
-
 	/** @var Nette\Database\Context */
 	private $database;
-
 
 	public function __construct(Nette\Database\Context $database)
 	{
 		$this->database = $database;
 	}
 
-
 	/**
 	 * Performs an authentication.
+	 *
 	 * @return Nette\Security\Identity
 	 * @throws Nette\Security\AuthenticationException
 	 */
@@ -40,21 +36,18 @@ class UserManager extends Nette\Object implements NS\IAuthenticator
 		list($username, $password) = $credentials;
 		$row = $this->database->table('users')
 			->where('username', $username)->fetch();
-
 		if (!$row) {
 			throw new NS\AuthenticationException('User not found.');
 		}
-
 		if (!NS\Passwords::verify($password, $row->password)) {
 			throw new NS\AuthenticationException('Invalid password.');
 		}
-
-		return new NS\Identity($row->id, $row->role, array('username' => $row->username));
+		return new NS\Identity($row->id, $row->role, ['username' => $row->username]);
 	}
-
 
 	/**
 	 * Adds new user.
+	 *
 	 * @param  string
 	 * @param  string
 	 * @return void
@@ -62,18 +55,16 @@ class UserManager extends Nette\Object implements NS\IAuthenticator
 	public function add($username, $password)
 	{
 		try {
-			$this->database->table(self::TABLE_NAME)->insert(array(
+			$this->database->table(self::TABLE_NAME)->insert([
 				self::COLUMN_NAME => $username,
 				self::COLUMN_PASSWORD_HASH => Passwords::hash($password),
-			));
+			]);
 		} catch (Nette\Database\UniqueConstraintViolationException $e) {
 			throw new DuplicateNameException;
 		}
 	}
-
 }
 
-
-
 class DuplicateNameException extends \Exception
-{}
+{
+}
