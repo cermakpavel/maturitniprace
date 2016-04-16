ded2a1150d9ba335e17ee57a7a5b18f5ca866000
Pavel Čermák <admin@cermakpavel.com>
2016-04-16 23:03:07 +0200

    New table comments

--- database/exported_database.sql
+++ database/exported_database.sql
@@ -2,10 +2,10 @@
 -- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 20, 2016 at 11:17 AM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 7.0.0
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Sob 16. dub 2016, 23:01
+-- Verze serveru: 10.1.10-MariaDB
+-- Verze PHP: 7.0.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `nette`
+-- Databáze: `test`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
+-- Struktura tabulky `comments`
 --
 
 CREATE TABLE `comments` (
@@ -32,13 +32,14 @@ CREATE TABLE `comments` (
   `name` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
   `content` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `approved` tinyint(4) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `posts`
+-- Struktura tabulky `posts`
 --
 
 CREATE TABLE `posts` (
@@ -49,38 +50,36 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `posts`
+-- Vypisuji data pro tabulku `posts`
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `created_at`) VALUES
-(7, 'Stránka', 'Stránečka', '2016-02-14 12:16:05'),
-(8, 'Nadpis 2', 'BlaBlaBla', '2016-02-14 12:18:24'),
-(9, 'Poslední přidaná', 'BlaBla', '2016-02-14 12:29:08');
+(13, 'Úvodní stránka', '\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet fringilla massa. Phasellus pharetra lacinia nisi eget faucibus. Phasellus sed mi tempus, porta nunc vel, vehicula eros. Nulla non quam fermentum est accumsan sollicitudin non porttitor orci. Donec at sem risus. Maecenas non dapibus velit, eu maximus risus. Aenean ipsum diam, auctor ac urna efficitur, egestas dignissim nulla. Curabitur ac neque ut sapien faucibus tristique. Sed hendrerit commodo erat, eu tristique magna mollis sed. Vivamus vitae venenatis orci, in sollicitudin leo. Proin luctus nibh turpis, in aliquet odio commodo vel. Nam mauris nunc, facilisis eget iaculis efficitur, venenatis vel ex.\n\nInteger malesuada erat vitae neque vehicula, vel lobortis risus laoreet. Maecenas porttitor tempor malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus imperdiet tortor ut tellus maximus, non accumsan lacus efficitur. Suspendisse convallis vitae urna vel efficitur. In vitae lorem aliquet, finibus libero vitae, ullamcorper diam. Nunc consequat nisi nisl, nec eleifend dui feugiat eu. Curabitur eu nunc odio. Integer rutrum, lorem non tempus varius, neque eros porttitor nisl, id placerat elit est at orci. Maecenas aliquam dictum mi, a rutrum justo porta a. Phasellus ut lacus sed orci posuere iaculis et ut urna.\n\nPhasellus fermentum, orci id tempor commodo, ex dui viverra arcu, non iaculis risus justo et magna. In suscipit eros non nunc vehicula, a tempus nisl congue. In hac habitasse platea dictumst. Aliquam eget est ornare, fringilla leo quis, malesuada ante. Curabitur augue turpis, cursus quis vehicula nec, vestibulum porttitor odio. Etiam iaculis ipsum sit amet fermentum auctor. Nullam eget tortor ante. Fusce rutrum quis augue ut fringilla. Nullam semper rhoncus vestibulum. Mauris nec metus at mauris viverra commodo. Proin porta libero nec tortor varius, id elementum sem laoreet. Nullam dictum sem at tincidunt semper. Nulla at turpis sollicitudin, convallis sem eget, varius lectus. Donec mattis libero at vehicula maximus. Nullam ut elit non ligula ultrices tempor id sed dui.\n\nVivamus tincidunt arcu id semper posuere. Nullam rutrum viverra odio vitae ultricies. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec mattis, lectus vel faucibus accumsan, erat quam dapibus mi, at vestibulum purus lorem id ligula. Etiam in suscipit odio, porta pulvinar neque. Pellentesque sagittis augue nibh, a convallis tellus sollicitudin sit amet. Aliquam erat volutpat. Aenean a accumsan tellus. Maecenas varius, quam eu lacinia facilisis, mi eros pellentesque velit, nec feugiat augue nibh non ante. Sed venenatis leo sit amet ornare feugiat. Donec blandit massa quis mi elementum, non feugiat sem fringilla. Ut purus quam, finibus sit amet bibendum ut, porta eget eros. Praesent tempor, dolor consectetur malesuada facilisis, risus nunc finibus nunc, vitae porta nulla mi a lacus. Nulla volutpat mi tortor, ac laoreet erat porta euismod.\n\nPhasellus nec facilisis elit. Sed tortor tortor, varius sit amet vulputate quis, feugiat et risus. Integer imperdiet nisl nec leo laoreet, a cursus mi rutrum. Ut sagittis eros quis metus vehicula egestas. Sed vel ornare nisi. Quisque tempor neque finibus erat pulvinar, nec egestas neque tristique. Praesent sed pretium dui. Donec maximus est non dictum ullamcorper. Nulla erat ligula, vestibulum eget sem sed, efficitur aliquet nunc. Maecenas odio enim, auctor et tellus commodo, tincidunt fringilla odio. Aenean pulvinar ullamcorper lectus a pretium. Etiam feugiat erat eget erat luctus, eget dignissim enim volutpat. Maecenas diam felis, consectetur in pulvinar vitae, commodo rhoncus massa. Cras vel aliquet augue, id sollicitudin risus. ', '2016-04-16 20:26:10');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `setting`
+-- Struktura tabulky `setting`
 --
 
 CREATE TABLE `setting` (
-  `onepage` tinyint(4) NOT NULL,
+  `onepage_layout` tinyint(4) NOT NULL,
   `id` tinyint(4) NOT NULL,
   `title` tinytext COLLATE utf8_czech_ci NOT NULL,
   `subtitle` mediumtext COLLATE utf8_czech_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
--- Dumping data for table `setting`
+-- Vypisuji data pro tabulku `setting`
 --
 
-INSERT INTO `setting` (`onepage`, `id`, `title`, `subtitle`) VALUES
-(1, 1, 'Ukázková stránka', 'Podtitulek, který není nikde vidět :D ');
+INSERT INTO `setting` (`onepage_layout`, `id`, `title`, `subtitle`) VALUES
+(0, 1, 'Ukázková stránka 2', 'Podtitulek, který není nikde vidět :D ');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktura tabulky `users`
 --
 
 CREATE TABLE `users` (
@@ -91,72 +90,72 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
--- Dumping data for table `users`
+-- Vypisuji data pro tabulku `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 (2, 'admin', '$2y$10$6ZYx4..5qfaLxkmEDkx/h.xOcC./GzvDQqR2bjMOzwkm0E6zug2zq', 'admin');
 
 --
--- Indexes for dumped tables
+-- Klíče pro exportované tabulky
 --
 
 --
--- Indexes for table `comments`
+-- Klíče pro tabulku `comments`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `post_id` (`post_id`);
 
 --
--- Indexes for table `posts`
+-- Klíče pro tabulku `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `setting`
+-- Klíče pro tabulku `setting`
 --
 ALTER TABLE `setting`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Klíče pro tabulku `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pro tabulky
 --
 
 --
--- AUTO_INCREMENT for table `comments`
+-- AUTO_INCREMENT pro tabulku `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `posts`
+-- AUTO_INCREMENT pro tabulku `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
--- AUTO_INCREMENT for table `setting`
+-- AUTO_INCREMENT pro tabulku `setting`
 --
 ALTER TABLE `setting`
   MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pro tabulku `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- Constraints for dumped tables
+-- Omezení pro exportované tabulky
 --
 
 --
--- Constraints for table `comments`
+-- Omezení pro tabulku `comments`
 --
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
