6552777c6e7d989d4e04605414dee2dafa70b359
Pavel Čermák <admin@cermakpavel.com>
2016-04-16 22:49:07 +0200

    Add new route

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -3,9 +3,8 @@
 namespace App;
 
 use Nette;
-use Nette\Application\Routers\RouteList;
 use Nette\Application\Routers\Route;
-
+use Nette\Application\Routers\RouteList;
 
 class RouterFactory
 {
@@ -20,13 +19,14 @@ class RouterFactory
 		$frontRouter[] = new Route('home', 'Homepage:');
 		$frontRouter[] = new Route('post', 'Post:');
 		$router[] = $adminRouter = new RouteList('Admin');
-		$adminRouter[] = new Route('admin', 'Sign:in');
+		$adminRouter[] = new Route('sign', 'Sign:in');
 		$adminRouter[] = new Route('dashboard', 'Dashboard:');
 		$adminRouter[] = new Route('logout', 'Sign:out');
 		$adminRouter[] = new Route('edit', 'Post:edit');
 		$adminRouter[] = new Route('', 'Post:delete');
 		$adminRouter[] = new Route('create', 'Post:create');
 		$adminRouter[] = new Route('setting', 'Setting:edit');
+		$adminRouter[] = new Route('comments', 'Comments:');
 		return $router;
 	}
 
