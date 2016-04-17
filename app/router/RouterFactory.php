cba185a19ffb5580e3da20b5b68f6292d677c600
Pavel Čermák <admin@cermakpavel.com>
2016-04-17 18:29:59 +0200

    Update routes

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -20,12 +20,12 @@ class RouterFactory
 		$adminRouter[] = new Route('dashboard', 'Dashboard:');
 		$adminRouter[] = new Route('logout', 'Sign:out');
 		$adminRouter[] = new Route('edit', 'Post:edit');
-		$adminRouter[] = new Route('', 'Post:delete');
+		$adminRouter[] = new Route('delete', 'Post:delete');
 		$adminRouter[] = new Route('create', 'Post:create');
 		$adminRouter[] = new Route('setting', 'Setting:edit');
 		$adminRouter[] = new Route('comments', 'Comments:');
 		$router[] = $frontRouter = new RouteList('Front');
-		$frontRouter[] = new Route('home', 'Homepage:');
+		$frontRouter[] = new Route('', 'Homepage:');
 		$frontRouter[] = new Route('post', 'Post:');
 		return $router;
 	}
